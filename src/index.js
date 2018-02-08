b6b7456f1d66ea83cdd90cdf41267a9ce2406e26
snakerabbit <alison.s.cheng@gmail.com>
2018-02-07 19:19:33 -0800

    added url to commentbox

--- src/index.js
+++ src/index.js
@@ -3,4 +3,7 @@ import ReactDOM from 'react-dom';
 import CommentBox from './CommentBox';
 
 
-ReactDOM.render(<CommentBox/>, document.getElementById('root'));
+ReactDOM.render(<CommentBox
+                url = 'http://localhost:3001/api/comments'
+                pollInterval={2000}/>,
+                document.getElementById('root'));
