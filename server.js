68d727c89cf38d660496867db67889bacd9e2c4d
snakerabbit <alison.s.cheng@gmail.com>
2018-02-07 18:36:33 -0800

    connected to mongodb

--- server.js
+++ server.js
@@ -6,18 +6,18 @@ var bodyParser = require('body-parser');
 var app = express();
 var router = express.Router();
 var port = process.env.API_PORT || 3001;
-
+mongoose.connect('mongodb://alisoncheng:password@ds141514.mlab.com:41514/mern');
 app.use(bodyParser.urlencoded({extended: true}));
 
-app.use(function(req, res, next) {
- res.setHeader('Access-Control-Allow-Origin', '*');
- res.setHeader('Access-Control-Allow-Credentials', 'true');
- res.setHeader('Access-Control-Allow-Methods', 'GET,HEAD,OPTIONS,POST,PUT,DELETE');
- res.setHeader('Access-Control-Allow-Headers', 'Access-Control-Allow-Headers, Origin,Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers');
- res.setHeader('Cache-Control', 'no-cache');
-});
+// app.use(function(req, res, next) {
+//  res.setHeader('Access-Control-Allow-Origin', '*');
+//  res.setHeader('Access-Control-Allow-Credentials', 'true');
+//  res.setHeader('Access-Control-Allow-Methods', 'GET,HEAD,OPTIONS,POST,PUT,DELETE');
+//  res.setHeader('Access-Control-Allow-Headers', 'Access-Control-Allow-Headers, Origin,Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers');
+//  res.setHeader('Cache-Control', 'no-cache');
+// });
 
-app.get('/', function(req, res){
+router.get('/', function(req, res){
   res.json({
     message: 'API Initialized!'
   });
